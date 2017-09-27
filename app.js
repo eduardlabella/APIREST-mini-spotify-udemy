ddcd64ecf323707e732714e7206324678b1a173a
Eduard Labella <labellaeduard@gmail.com>
2017-09-27 18:26:10 -0300

    Starting API RestFul. minispotify. Using MEAN.

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+//cargar rutas
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//configurar cabeceras http
+
+//rutas base
+
+app.get('/pruebas', function(req,res){
+  res.status(200).send({message: 'Bienvenido al curso de victorroblesweb.es'});
+});
+
+module.exports = app;
