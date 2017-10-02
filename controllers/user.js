5c8f47e7abd595fa48bb4cb493ed0b77175e4427
Eduard Labella <labellaeduard@gmail.com>
2017-10-02 10:32:23 -0300

    Adding middlewares,uploadUsers and uploadImage

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,11 @@
 'use strict'
-var bcrypt = require('bcrypt-nodejs');
 
+var fs = require('fs');
+var path = require('path');
+var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 var jwt = require('../services/jwt');
+
 
 //Primera prueba
 function pruebas(req,res){
@@ -17,8 +20,6 @@ function saveUser(req,res){
 
   var params = req.body;
 
-
-  console.log(params);
   user.name = params.name;
   user.surname = params.surname;
   user.email = params.email;
@@ -87,8 +88,96 @@ function loginUser(req,res){
     }
   })
 }
+
+//Update User
+
+function updateUser(req,res){
+  var userId = req.params.id;
+  var update = req.body;
+
+  User.findByIdAndUpdate(userId,update, (err, userUpdated) => {
+    if(err){
+      res.status(500).send({message: 'Error al actualizar el usuario'});
+
+    }else{
+      if(!userUpdated){
+        res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+
+      }else{
+        res.status(200).send({user: userUpdated});
+      }
+
+
+    }
+
+
+  });
+
+
+
+
+};
+
+//Upload Image
+
+function uploadImage(req,res){
+
+  var userId = req.params.id;
+  var file_name = 'No subido...';
+
+  if (req.files) {
+    var file_path = req.files.image.path;
+    var file_split = file_path.split('/');
+    var file_name = file_split[2];
+
+    var ext_split = file_name.split('.');
+    var file_ext = ext_split[1];
+
+    if (file_ext == 'JPG' || file_ext == 'png' || file_ext == 'png'|| file_ext == 'jpg') {
+      User.findByIdAndUpdate(userId, {image: file_name},(err,userUpdated) => {
+        if(!userUpdated){
+          res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+
+        }else{
+          res.status(200).send({user: userUpdated});
+        }
+
+      });
+
+    }else {
+      res.status(200).send({message: 'Extensión del archivo no valida'});
+    }
+
+    console.log(ext_split);
+
+  }else {
+    res.status(200).send({message: 'No has subido ninguna imagen'});
+  }
+
+
+
+}
+
+
+function getImageFile(req,res){
+
+  var imageFile = req.params.imageFile;
+  var path_file = './uploads/users/'+imageFile;
+
+  fs.exists(path_file, function(exists){
+    if(exists){
+      res.sendFile(path.resolve(path_file));
+    }else {
+      res.status(200).send({message: 'No existe la imagen...'});
+    }
+  });
+}
+
 module.exports = {
   pruebas,
   saveUser,
-  loginUser
+  loginUser,
+  updateUser,
+  uploadImage,
+  getImageFile
 };
