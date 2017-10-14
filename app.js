8a268c2a51c29f57bbe832bdb8d61b186d9b5fdf
Eduard Labella <labellaeduard@gmail.com>
2017-10-14 10:26:13 -0300

    Back-End Completed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var app = express();
 var user_routes = require('./routes/user');
 var artist_routes = require('./routes/artist');
 var album_routes = require('./routes/album');
+var song_routes = require('./routes/song');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
@@ -17,4 +18,5 @@ app.use(bodyParser.json());
 app.use('/api', user_routes);
 app.use('/api', artist_routes);
 app.use('/api', album_routes);
+app.use('/api',song_routes);
 module.exports = app;
